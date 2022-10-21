9f02e1920a6995610fd7ec50e3e60142ff5209ff
Phanuwat_emotin <phanuwat_panya@elearning.cmu.ac.th>
2022-10-21 10:34:21 +0700

    fix ip

--- src/main/java/se331/rest/Lab08Application.java
+++ src/main/java/se331/rest/Lab08Application.java
@@ -18,7 +18,7 @@ public class Lab08Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","http://3.82.204.86:8001")
+                        .allowedOrigins("http://localhost:3000","http://54.164.28.255:8001")
                         .exposedHeaders("x-total-count");
 
             }
